e40680414cd4ea9ec7b3c253c8fd8a65c08417a0
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-01-20 17:59:14 -0500

    creation of the paddle that responds to key presses

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.screensize(800, 600)
 screen.bgcolor("black")
 screen.title("pong")
+screen.tracer(0)
 
 
-POSITIONS = [(350,0),(350,20), (350,40), (350,-20), (350,-40)]
-segments = []
-for position in POSITIONS:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.pu()
-    segment.goto(position)
-    
-    
-    
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_len=1, stretch_wid = 5)
+paddle.pu()
+paddle.goto(350,0)
+       
+def go_up():
+    paddle.goto(paddle.xcor(),paddle.ycor() +20)
+   
+def go_down():
+    paddle.goto(paddle.xcor(),paddle.ycor() -20)
+
+screen.listen()  
+screen.onkey(go_up,"Up")
+screen.onkey(go_down,"Down") 
+ 
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
